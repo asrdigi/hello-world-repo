ab5a992987c57df5a7e3906e7360980e42144084
Annapureddy Srinivas Reddy <digitech1993@gmail.com>
2022-06-06 01:48:43 +0530

    second commit

--- src/com/sapient/asde/ui/HelloWorld.java
+++ src/com/sapient/asde/ui/HelloWorld.java
@@ -3,5 +3,6 @@ package com.sapient.asde.ui;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Than Q");
     }
 }
